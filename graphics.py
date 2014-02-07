df985b8d5857092a110e2eb34ec728fd02197ccb
Lucas Silveira Melo <lucas.silveiramelo@gmail.com>
2014-02-07 11:35:38 -0300

    Commit inicial do projeto SimuladorA

---
+++ graphics.py
@@ -0,0 +1,369 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PySide import QtCore, QtGui
+import math
+import sys
+
+class Edge(QtGui.QGraphicsLineItem):
+    '''
+        Classe que implementa o objeto Edge que liga dois objetos Node um ao outro
+    '''
+    def __init__(self, w1, w2, parent=None, scene=None):
+        '''
+            Metodo inicial da classe Edge
+            Recebe como parâmetros os objetos Node Inicial e Final
+            Define o objeto QtCore.QLineF que define a linha que representa o objeto QtGui.QGraphicsLineItem
+        '''
+        super(Edge, self).__init__()
+        
+        self.w1 = w1
+        self.w1.addEdge(self) # adiciona o objeto Edge a lista de Edges do objeto w1
+        self.w2 = w2
+        self.w2.addEdge(self) # adiciona o objeto Edge a lista de Edges do objeto w2
+        
+        line = QtCore.QLineF(self.w1.pos(), self.w2.pos())
+        self.setLine(line)
+        self.setZValue(-1)
+    
+    def updatePosition(self):
+        '''
+            Metodo de atualizacao da posicao do objeto edge implementado pela classe Edge
+            Sempre que um dos objetos Nodes w1 ou w2 modifica sua posicao este metodo 
+            e chamado para que o objeto edge possa acompanhar o movimento dos Objetos Node
+        '''
+        if not self.w1 or not self.w2:
+            return
+        
+        line = QtCore.QLineF(self.w1.pos(), self.w2.pos())
+        length = line.length()
+        
+        if length == 0.0:
+            return
+        
+        self.prepareGeometryChange()
+        self.setLine(line)
+    
+    def setColor(self, color):
+        self.setPen(QtGui.QPen(color))
+        
+    def boundingRect(self):
+        '''
+            Metodo de definicao da borda do objeto edge implementado pela classe Edge
+        '''
+        extra = (self.pen().width() + 10) / 2.0
+        p1 = self.line().p1()  # ponto inicial do objeto QtCore.QLineF associado ao objeto QtGui.QGraphicsLine
+        p2 = self.line().p2()  # ponto final do objeto QtCore.QLineF associado ao objeto QtGui.QGraphicsLine
+        
+        return QtCore.QRectF(p1,                                        # topleft associada ao objeto QRectF 
+                                    QtCore.QSizeF(p2.x() - p1.x(),  # comprimento no eixo x associado ao objeto QtCore.QSizeF
+                                                  p2.y() - p1.y()   # comprimento no eixo y associado ao objeto QtCore.QSizeF
+                                                  )                     # size associado ao objeto QRectF
+                                 ).normalized().adjusted(-extra, -extra, extra, extra)
+
+    def paint(self, painter, option, widget):
+        '''
+            Metodo de desenho do objeto edge implementado pela classe Edge
+        '''
+        
+        if (self.w1.collidesWithItem(self.w2)):
+            return
+        
+        w1 = self.w1
+        w2 = self.w2
+        
+        line = QtCore.QLineF(self.mapFromItem(self.w1, self.w1.rect.center()) , self.mapFromItem(self.w2, self.w2.rect.center()))
+        
+        p1 = w2.rect.topLeft() + w1.pos()
+        
+        intersectPoint = QtCore.QPointF()
+        
+            
+        self.setLine(line)
+        
+        painter.setPen(QtGui.QPen(QtCore.Qt.black,  # QPen Brush
+                               1,  # QPen width
+                               QtCore.Qt.SolidLine,  # QPen style
+                               QtCore.Qt.SquareCap,  # QPen cap style
+                               QtCore.Qt.RoundJoin)  # QPen join style
+                       )
+        painter.setBrush(QtCore.Qt.black)
+        painter.drawLine(self.line())
+
+
+class Text(QtGui.QGraphicsTextItem):
+    '''
+        Classe que implementa o objeto Text Generico
+    '''
+    
+    def __init__(self):
+        pass
+
+class Node(QtGui.QGraphicsRectItem):
+    '''
+       Classe que implementa o objeto Node Generico 
+    '''
+    
+    # tipos de itens possiveis
+    Subestacao, Religador, Barra, Agent = range(4)
+    
+    def __init__(self, nodeType, nodeMenu, parent=None, scene=None):
+    
+        '''
+            Metodo inicial da classe Node
+            Recebe como parâmetros os objetos nodeType que define o tipo de Node desejado e x, y a posicao do objeto Node
+            Define o objeto QtCore.QRectF que define o retangulo que representa o objeto QtGui.QGraphicsRectItem
+        '''
+        super(Node, self).__init__()
+        
+        self.edges = []
+        
+        self.nodeType = nodeType
+        
+        # caso o item a ser inserido seja do tipo subestacao
+        if self.nodeType == self.Subestacao:
+            self.rect = QtCore.QRectF(0, 0, 50.0, 50.0)
+        # caso o item a ser inserido seja do tipo religador
+        elif self.nodeType == self.Religador:
+            self.rect = QtCore.QRectF(0, 0, 50.0, 50.0)
+        # caso o item a ser inserido seja do tipo barra
+        elif self.nodeType == self.Barra:
+            self.rect = QtCore.QRectF(0, 0, 10.0, 100.0)
+        # caso o item a ser inserido seja do tipo agent
+        elif self.nodeType == self.Agent:
+            self.rect = QtCore.QRectF(0, 0, 50.0, 50.0)
+            
+        self.setRect(self.rect)
+        self.setFlag(QtGui.QGraphicsItem.ItemIsMovable, True)
+        self.setFlag(QtGui.QGraphicsItem.ItemIsSelectable, True)
+        self.setZValue(0)
+    
+    def removeEdge(self, edge):
+        '''
+            Metodo de remocao de objetos edge associados ao objeto node
+        '''
+        try:
+            self.edges.remove(edge)
+        except ValueError:
+            pass
+ 
+    def removeEdges(self):
+        '''
+            Metodo de remocao de todos bjetos edge associados ao objeto node
+        '''
+        for edge in self.edges[:]:
+            edge.startItem().removeEdge(edge)
+            edge.endItem().removeEdge(edge)
+            self.scene().removeItem(edge)
+ 
+    def addEdge(self, edge):
+        '''
+            Metodo de adicao de objetos edge associados ao objeto node
+        '''
+        self.edges.append(edge)
+    
+    def boundingRect(self):
+        '''
+            Metodo que especifica a borda do objeto node
+        '''
+        extra = 5.0
+        return self.rect.adjusted(-extra, -extra, extra, extra)
+        
+    def paint(self, painter, option, widget):
+        '''
+            Metodo de desenho do objeto node implementado pela classe Node
+        '''
+        
+        # caso o item a ser inserido seja do tipo subestacao
+        if self.nodeType == self.Subestacao:
+            painter.setPen(QtGui.QPen(QtCore.Qt.black, 1.5))
+            painter.setBrush(QtCore.Qt.white)
+        # caso o item a ser inserido seja do tipo religador
+        elif self.nodeType == self.Religador:
+            painter.setPen(QtGui.QPen(QtCore.Qt.black, 1.5))
+            painter.setBrush(QtCore.Qt.white)
+        # caso o item a ser inserido seja do tipo barra
+        elif self.nodeType == self.Barra:
+            painter.setPen(QtGui.QPen(QtCore.Qt.black, 1.5))
+            painter.setBrush(QtCore.Qt.black)
+        # caso o item a ser inserido seja do tipo agent
+        elif self.nodeType == self.Agent:
+            painter.setPen(QtGui.QPen(QtCore.Qt.black, 1.5))
+            painter.setBrush(QtCore.Qt.white)
+        
+        painter.drawRect(self.rect)
+        
+    def itemChange(self, change, value):
+        '''
+            Metodo que detecta mudancas na posicao do objeto node
+        '''
+        if change == QtGui.QGraphicsItem.ItemPositionChange:
+            for edge in self.edges:
+                edge.updatePosition()
+        return QtGui.QGraphicsItem.itemChange(self, change, value)
+        
+class SceneWidget(QtGui.QGraphicsScene):
+    '''
+        Classe que implementa o container Grafico onde os
+        widgets residirao
+    '''
+    
+    # tipos de modos de iteracao com o diagrama grafico
+    InsertItem, InsertLine, InsertText, MoveItem  = range(4)
+    
+    # signal definido para a classe SceneWidget enviado quando um item é inserido no diagrama grafico
+    itemInserted = QtCore.Signal(int)
+    
+    def __init__(self):
+        
+        super(SceneWidget, self).__init__()
+        
+        self.setSceneRect(0, 0, 800, 800)
+        
+        self.myMode = self.MoveItem
+        self.myItemType = Node.Subestacao
+        self.line = None
+        self.textItem = None
+
+    def mousePressEvent(self, mouseEvent):
+        '''
+            Este metodo define as acoes realizadas quando um evento do tipo
+            mousePress e detectado no diagrama grafico
+        '''
+        
+        if (mouseEvent.button() != QtCore.Qt.LeftButton):
+            return
+
+        if self.myMode == self.InsertItem:
+            
+            item = Node(self.myItemType, None)
+            self.addItem(item)
+            item.setPos(mouseEvent.scenePos())
+            self.itemInserted.emit(self.myItemType)
+            
+        elif self.myMode == self.InsertLine:
+            
+            self.line = QtGui.QGraphicsLineItem(
+                                                QtCore.QLineF(
+                                                              mouseEvent.scenePos(),
+                                                              mouseEvent.scenePos()
+                                                              )
+                                                )
+            self.line.setPen(
+                             QtGui.QPen(QtCore.Qt.black, 2)
+                             )
+            self.addItem(self.line)
+            
+        elif self.myMode == self.InsertText:
+            textItem = Text()
+            textItem.setFont(self.myFont)
+            textItem.setTextInteractionFlags(QtCore.Qt.TextEditorInteraction)
+            textItem.setZValue(1000.0)
+            textItem.lostFocus.connect(self.editorLostFocus)
+            textItem.selectedChange.connect(self.itemSelected)
+            self.addItem(textItem)
+            textItem.setDefaultTextColor(self.myTextColor)
+            textItem.setPos(mouseEvent.scenePos())
+            self.textInserted.emit(textItem)
+
+        super(SceneWidget, self).mousePressEvent(mouseEvent)
+
+        return
+    
+    def mouseMoveEvent(self, mouseEvent):
+        '''
+            Este medodo define as acoes realizadas quando um evento do tipo mouseMove
+            e detectado no diagrama grafico. Neste caso desenha uma linha quando o modo
+            self.InsertLine está ativado
+        '''
+        if self.myMode == self.InsertLine and self.line:
+            newLine = QtCore.QLineF(self.line.line().p1(), mouseEvent.scenePos())
+            self.line.setLine(newLine)
+        elif self.myMode == self.MoveItem:
+            super(SceneWidget, self).mouseMoveEvent(mouseEvent)
+    
+    def mouseReleaseEvent(self, mouseEvent):
+        '''
+            Este medodo define as acoes realizadas quando um evento do tipo mouseRealese
+            e detectado no diagrama grafico. Neste caso conecta os dois elementos que estão ligado
+            pela linha criada no evento mousePress
+        '''
+        if self.line and self.myMode == self.InsertLine:
+            startItems = self.items(self.line.line().p1())
+            if len(startItems) and startItems[0] == self.line:
+                startItems.pop(0)
+            endItems = self.items(self.line.line().p2())
+            if len(endItems) and endItems[0] == self.line:
+                endItems.pop(0)
+
+            self.removeItem(self.line)
+            self.line = None
+
+            if len(startItems) and len(endItems) and \
+                    isinstance(startItems[0], Node) and \
+                    isinstance(endItems[0], Node) and \
+                    startItems[0] != endItems[0]:
+                startItem = startItems[0]
+                endItem = endItems[0]
+                edge = Edge(startItem, endItem)
+                edge.setColor(QtCore.Qt.black)
+                startItem.addEdge(edge)
+                endItem.addEdge(edge)
+                self.addItem(edge)
+                edge.updatePosition()
+                                                                                                          
+        self.line = None
+        self.itemInserted.emit(3)
+        super(SceneWidget, self).mouseReleaseEvent(mouseEvent)
+    
+    def setItemType(self, type):
+        '''
+            Define em qual tipo de item sera inserido no diagrama grafico assim que um evento
+            do tipo mousePress for detectado, podendo ser:
+            Node.Subestacao
+            Node.Religador
+            Node.Barra
+            Node.Agent
+        '''
+        self.myItemType = type
+    
+    def setMode(self, mode):
+        '''
+            Define em qual modo 
+        '''
+        self.myMode = mode
+
+            
+class ViewWidget(QtGui.QGraphicsView):
+    '''
+        Esta classe implementa o container QGraphicsView
+        onde residira o objeto QGraphicsScene
+    '''
+    def __init__(self, scene):
+        
+        super(ViewWidget, self).__init__(scene)
+        
+        self.setCacheMode(QtGui.QGraphicsView.CacheBackground)
+        self.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.setTransformationAnchor(QtGui.QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QtGui.QGraphicsView.AnchorViewCenter)
+
+    def wheelEvent(self, event):
+        self.scaleView(math.pow(2.0, -event.delta() / 240.0))
+    
+    def scaleView(self, scaleFactor):
+        factor = self.matrix().scale(scaleFactor, scaleFactor).mapRect(QtCore.QRectF(0, 0, 1, 1)).width()
+
+        if factor < 0.5 or factor > 3:
+            return
+
+        self.scale(scaleFactor, scaleFactor)
+
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    scene = SceneWidget()
+    widget = ViewWidget(scene)
+    widget.show()
+    sys.exit(app.exec_())
---
+++ simulador.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'simulador.ui'
+#
+# Created: Sun Jan 26 16:58:45 2014
+#      by: Lucas S Melo
+#
+
+from PySide import QtCore, QtGui
+from graphics import SceneWidget, ViewWidget
+import sys
+
+class Ui_MainWindow(object):
+    '''
+        Esta classe implementa a inteface grafica do simulador
+    '''
+    
+    def setupUi(self, MainWindow):
+        '''
+            Este metodo implementa os componentes da inteface grafica
+        '''
+        
+        # objeto define a janela pricipal do aplicativo
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(900, 700)
+        
+        # define o widget central do aplicativo
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        
+        # define o tipo de layout do widget central como gridLayout
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        
+        # define a classe SceneWidget e ViewWidget como containers dos widgets
+        self.sceneWidget = SceneWidget()
+        self.graphicsView = ViewWidget(self.sceneWidget)
+        self.graphicsView.setMinimumSize(QtCore.QSize(256, 0))
+        self.graphicsView.setObjectName("graphicsView")
+        
+        # adiciona os sinais ao objeto sceneWidget
+        self.sceneWidget.itemInserted.connect(self.itemInserted)
+        
+        
+        # conecta os botoes aos signals da sceneWidget
+        #self.sceneWidget.InsertItem.connect(self.itemInserted)
+        
+        # seta o objeto QGraphicsView no gridLayout
+        self.gridLayout.addWidget(self.graphicsView, 0, 0)
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+        # define a barra de menus
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        
+        # define a barra de status
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        
+        # define a barra de ferramentas
+        self.toolBar = QtGui.QToolBar(MainWindow)
+        self.toolBar.setEnabled(True)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        
+        # define o widget dockWidget dockWidget_Buttons e configura seu conteudo dockWidget_Buttons_Contents
+        self.dockWidget_Buttons = QtGui.QDockWidget(MainWindow)
+        self.dockWidget_Buttons.setObjectName("dockWidget_Buttons")
+        self.dockWidget_Buttons_Contents = QtGui.QWidget()
+        self.dockWidget_Buttons_Contents.setMinimumWidth(230)
+        self.dockWidget_Buttons_Contents.setObjectName("dockWidget_Buttons_Contents")
+        
+        #  define o layput dos botoes no dockWidget  gridLayout
+        self.gridLayout_dockWidget = QtGui.QGridLayout(self.dockWidget_Buttons_Contents)
+        self.gridLayout_dockWidget.setObjectName("gridLayout_dockWidget")
+        
+        # define o objeto QToolBox que comportara as abas de botoes
+        self.toolBox = QtGui.QToolBox(self.dockWidget_Buttons_Contents)
+        self.toolBox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.toolBox.setObjectName("toolBox")
+        
+        # define a primeira pagina do dockWidget
+        self.page_1 = QtGui.QWidget()
+        self.page_1.setGeometry(QtCore.QRect(0, 0, 100, 50))
+        
+        # configura a primeira pagina do dockWidget
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.page_1.sizePolicy().hasHeightForWidth())
+        self.page_1.setSizePolicy(sizePolicy)
+        self.page_1.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.page_1.setAutoFillBackground(True)
+        self.page_1.setObjectName("page_1")
+        
+        # define o Layout da primeira pagina do dockWidget
+        self.gridlayout_page_1 = QtGui.QGridLayout(self.page_1)
+        self.gridlayout_page_1.setSizeConstraint(QtGui.QLayout.SetFixedSize)
+        self.gridlayout_page_1.setObjectName("gridlayout_page_1")
+        
+        # define os botoes da primeira pagina do dockWidget e insere no FormLayout
+        self.substationButton = QtGui.QPushButton(self.page_1)
+        self.substationButton.setObjectName("substationButton")
+        self.substationButton.setCheckable(True)
+        self.busButton = QtGui.QPushButton(self.page_1)
+        self.busButton.setObjectName("busButton")
+        self.busButton.setCheckable(True)
+        self.recloserButton = QtGui.QPushButton(self.page_1)
+        self.recloserButton.setObjectName("recloserButton")
+        self.recloserButton.setCheckable(True)
+        self.lineButton = QtGui.QPushButton(self.page_1)
+        self.lineButton.setObjectName("lineButton")
+        self.lineButton.setCheckable(True)        
+        
+        
+        # define o grupo de botoes da pagina 1 do notebook
+        self.buttonGroup = QtGui.QButtonGroup()
+        self.buttonGroup.addButton(self.substationButton, 0)
+        self.buttonGroup.addButton(self.recloserButton, 1)
+        self.buttonGroup.addButton(self.busButton, 2)
+        self.buttonGroup.addButton(self.lineButton, 3)
+        self.buttonGroup.setExclusive(False)
+        
+        self.buttonGroup.buttonClicked[int].connect(self.buttonGroupClicked)
+        
+        # define labels da primeira pagina do dockWidget
+        self.substationLabel = QtGui.QLabel('')
+        self.substationLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        self.substationLabel.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.substationLabel.setObjectName("substationLabel")
+        self.recloserLabel = QtGui.QLabel('')
+        self.recloserLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        self.recloserLabel.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.recloserLabel.setObjectName("recloserLabel")
+        self.busLabel = QtGui.QLabel('')
+        self.busLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        self.busLabel.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.busLabel.setObjectName("busLabel")
+        self.lineLabel = QtGui.QLabel('')
+        self.lineLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        self.lineLabel.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.lineLabel.setObjectName("lineLabel")
+        
+        # adiciona os botoes ao gridLayout_3
+        self.gridlayout_page_1.addWidget(self.substationButton, 0, 0)
+        self.gridlayout_page_1.addWidget(self.recloserButton, 0, 1)
+        self.gridlayout_page_1.addWidget(self.substationLabel, 1, 0)
+        self.gridlayout_page_1.addWidget(self.recloserLabel, 1, 1)
+        self.gridlayout_page_1.addWidget(self.busButton, 2, 0)
+        self.gridlayout_page_1.addWidget(self.lineButton, 2, 1)
+        self.gridlayout_page_1.addWidget(self.busLabel, 3, 0)
+        self.gridlayout_page_1.addWidget(self.lineLabel, 3, 1)
+        
+        # adiciona o gridLayout_3 a pagina_1 do dockWidget
+        self.page_1.setLayout(self.gridlayout_page_1)
+        
+        # seta a primeira pagina do dockWidget 
+        self.toolBox.addItem(self.page_1, "")
+        
+        # define a segunda pagina do dockWidget
+        self.page_2 = QtGui.QWidget()
+        self.page_2.setGeometry(QtCore.QRect(0, 0, 100, 50))
+        self.page_2.setObjectName("page_2")
+        self.toolBox.addItem(self.page_2, "")
+        
+        
+        self.gridLayout_dockWidget.addWidget(self.toolBox, 0, 0)
+        self.dockWidget_Buttons.setWidget(self.dockWidget_Buttons_Contents)
+        
+        MainWindow.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.dockWidget_Buttons)
+        
+        
+        # configura os botoes da barra de ferramentas
+        self.actionExit = QtGui.QAction(MainWindow)
+        
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionExit.setIcon(icon)
+        self.actionExit.setObjectName("actionExit")
+        self.toolBar.addAction(self.actionExit)
+        
+        # configuracoes adicionais
+        self.retranslateUi(MainWindow)
+        self.toolBox.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+    def itemInserted(self, itemType):
+        '''
+            Callback chamada no momento em que um item e iserido
+            no diagrama grafico
+        '''
+        self.buttonGroup.button(itemType).setChecked(False)
+        self.sceneWidget.setMode(self.sceneWidget.MoveItem)
+    
+    def buttonGroupClicked(self, id):
+        '''
+            Callback chamada no momento em que um botao de insersao
+            de itens e pressionado 
+        '''
+        print id
+        buttons = self.buttonGroup.buttons()
+        for button in buttons:
+            if self.buttonGroup.button(id) != button:
+                button.setChecked(False)
+        
+        if id == 3:
+            self.sceneWidget.setMode(SceneWidget.InsertLine)
+        else:
+            self.sceneWidget.setItemType(id)
+            self.sceneWidget.setMode(SceneWidget.InsertItem)
+
+    def retranslateUi(self, MainWindow):
+        
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.toolBar.setWindowTitle(QtGui.QApplication.translate("MainWindow", "toolBar", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.substationButton.setText(QtGui.QApplication.translate("MainWindow", "Subestação", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.busButton.setText(QtGui.QApplication.translate("MainWindow", "Barra", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.busLabel.setText(QtGui.QApplication.translate("MainWindow", "Barra", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.substationLabel.setText(QtGui.QApplication.translate("MainWindow", "Subestação", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.recloserButton.setText(QtGui.QApplication.translate("MainWindow", "Religador", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.recloserLabel.setText(QtGui.QApplication.translate("MainWindow", "Religador", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.lineButton.setText(QtGui.QApplication.translate("MainWindow", "Linha", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.lineLabel.setText(QtGui.QApplication.translate("MainWindow", "Linha", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.toolBox.setItemText(self.toolBox.indexOf(self.page_1), QtGui.QApplication.translate("MainWindow", "Pagina 1", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.toolBox.setItemText(self.toolBox.indexOf(self.page_2), QtGui.QApplication.translate("MainWindow", "Pagina 2", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.actionExit.setText(QtGui.QApplication.translate("MainWindow", "Exit", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.actionExit.setToolTip(QtGui.QApplication.translate("MainWindow", "Exit", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.actionExit.setShortcut(QtGui.QApplication.translate("MainWindow", "4, Backspace", None, QtGui.QApplication.UnicodeUTF8))
+
+
+class ControlMainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(ControlMainWindow, self).__init__(parent)
+        self.ui =  Ui_MainWindow()
+        self.ui.setupUi(self)
+   
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    mySW = ControlMainWindow()
+    mySW.show()
+    sys.exit(app.exec_())
