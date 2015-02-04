e56786038d65bbd1f3a16624209223b3897e60f8
mateus <mats347@gmail.com>
2015-02-04 10:29:31 -0300

    Correção de bugs menores; Adequação do código ao padrão pep8

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from xml.dom import minidom
 from PySide import QtCore, QtGui
 from graphics import Node, Edge
 
+
 class DiagramToXML(ElementTree.Element):
     '''
         Esta classe possui as funções que armazenam as informações
@@ -15,105 +16,116 @@ class DiagramToXML(ElementTree.Element):
             Função que inicializa o objeto criado pela classe DiagramToXML
         '''
         super(DiagramToXML, self).__init__('items')
-        
+
         self.scene = scene
         lista = self.scene.items()
         lista.reverse()
         for item in self.scene.items():
             if isinstance(item, Node):
-                node = ElementTree.Element('node', attrib={'type' : str(item.myItemType) })
+                node = ElementTree.Element(
+                    'node', attrib={'type': str(item.myItemType)})
                 id = ElementTree.Element('id')
                 id.text = str(item.id)
                 node.append(id)
-                
+
                 x = ElementTree.Element('x')
                 x.text = str(item.scenePos().x())
                 node.append(x)
-                
+
                 y = ElementTree.Element('y')
                 y.text = str(item.scenePos().y())
                 node.append(y)
-                
+
                 width = ElementTree.Element('width')
                 width.text = str(item.rect().width())
                 node.append(width)
-                
+
                 height = ElementTree.Element('height')
                 height.text = str(item.rect().height())
                 node.append(height)
-                
+
                 self.append(node)
         for item in lista:
-            
+
             if isinstance(item, Edge):
                 edge = ElementTree.Element('edge')
                 w1 = ElementTree.Element('w1')
                 w1.text = str(item.w1.id)
-                
-                
+
                 w2 = ElementTree.Element('w2')
                 w2.text = str(item.w2.id)
+
                 edge.append(w1)
                 edge.append(w2)
                 self.append(edge)
-                print str(item.w1.id),str(item.w2.id)
-    
-    def writeXML(self, path):
+                print str(item.w1.id), str(item.w2.id)
+
+    def write_xml(self, path):
         '''
             Função que cria o arquivo XML na localização indicada pelo
             argumento path
         '''
-        xmlString = ElementTree.tostring(self)
-        domElement = (minidom.parseString(xmlString))
+        xml_string = ElementTree.tostring(self)
+        dom_element = (minidom.parseString(xml_string))
         f = open(path, 'w')
-        f.write(domElement.toprettyxml())
+        f.write(dom_element.toprettyxml())
         f.close()
+
 
 class XMLToDiagram():
     '''
-        Classe que realiza a convesão do arquivo XML com as informações do diagrama
-        em um diagrama gráfico interativo
+        Classe que realiza a conversão do arquivo XML com as informações do 
+        diagrama em um diagrama gráfico interativo.
     '''
-    def __init__(self, scene, filePath):
+
+    def __init__(self, scene, file_path):
         self.scene = scene
-        self.filePath = filePath
-        
-        xmlTree = ElementTree.parse(self.filePath)
-        xmlElement = xmlTree.getroot()
-        print xmlElement
+        self.file_path = file_path
+
+        xml_tree = ElementTree.parse(self.file_path)
+        xml_element = xml_tree.getroot()
+        print xml_element
         self.scene.clear()
-        for child in xmlElement:
-            
+        for child in xml_element:
+
             if child.tag == 'node':
-                
+
                 if child.attrib['type'] == '0':
-                    item = Node(int(child.attrib['type']), self.scene.mySubstationMenu)
+                    item = Node(
+                        int(child.attrib['type']), self.scene.mySubstationMenu)
                     self.scene.addItem(item)
-                    item.setPos(float(child.find('x').text), float(child.find('y').text))
+                    item.setPos(
+                        float(child.find('x').text), float(
+                            child.find('y').text))
                     item.id = int(child.find('id').text)
-                    
-                    
+
                 elif child.attrib['type'] == '1':
-                    item = Node(int(child.attrib['type']), self.scene.myRecloserMenu)
+                    item = Node(
+                        int(child.attrib['type']), self.scene.myRecloserMenu)
                     item.id = int(child.find('id').text)
-                    item.setPos(float(child.find('x').text), float(child.find('y').text))                    
+                    item.setPos(float(child.find('x').text), float(
+                        child.find('y').text))
                     self.scene.addItem(item)
-                    
                 elif child.attrib['type'] == '2':
-                    item = Node(int(child.attrib['type']), self.scene.myBusMenu)
-                    item.setPos(float(child.find('x').text), float(child.find('y').text))
+                    item = Node(int(
+                        child.attrib['type']), self.scene.myBusMenu)
+                    item.setPos(float(child.find('x').text), float(
+                        child.find('y').text))
                     item.id = int(child.find('id').text)
-                    item.setRect(0, 0, float(child.find('width').text), float(child.find('height').text))
+                    item.setRect(
+                        0, 0, float(child.find('width').text), float(
+                            child.find('height').text))
                     self.scene.addItem(item)
-                    
+
                 elif child.attrib['type'] == '3':
                     item = Node(int(child.attrib['type']), None)
-                    item.setPos(float(child.find('x').text), float(child.find('y').text))
+                    item.setPos(
+                        float(child.find('x').text), float(
+                            child.find('y').text))
                     item.id = int(child.find('id').text)
                     self.scene.addItem(item)
-                    
+
             elif child.tag == 'edge':
-    
                 for item in self.scene.items():
                     if isinstance(item, Node) and item.id == int(child.find('w1').text):
                         w1 = item
@@ -121,8 +133,7 @@ class XMLToDiagram():
                     elif isinstance(item, Node) and item.id == int(child.find('w2').text):
                         w2 = item
                         print item.id
-                print w1.id,w2.id
-                edge = Edge(w1, w2,self.scene.myLineMenu)
+
+                edge = Edge(w1, w2, self.scene.myLineMenu)
                 self.scene.addItem(edge)
-                self.scene.addItem(edge.ghostRetItem)
-                #edge.updateRet()
+                self.scene.addItem(edge.GhostRetItem)
